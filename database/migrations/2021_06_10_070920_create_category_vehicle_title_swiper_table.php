2fbf06a4369874243995b114742ac5484da5a7c4
h-alimohammadi <hosein.alimohammadi73@gmail.com>
2023-04-19 00:17:18 -0700

    create project atiyar

---
+++ database/migrations/2021_06_10_070920_create_category_vehicle_title_swiper_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryVehicleTitleSwiperTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+//        Schema::connection('mysql-vehicle')->create('category_vehicle_title_swiper', function (Blueprint $table) {
+        Schema::create('category_vehicle_title_swiper', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_vehicle_title_swiper');
+//        Schema::connection('mysql-vehicle')->dropIfExists('category_vehicle_title_swiper');
+    }
+}
