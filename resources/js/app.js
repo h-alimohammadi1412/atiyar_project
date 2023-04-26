b5c1e6764265f2e0f6866d2294480cd2147e0f05
h-alimohammadi <hosein.alimohammadi73@gmail.com>
2023-04-26 06:08:57 -0700

    add spa page admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,16 @@ require('./bootstrap');
 require('chart.js');
 require('alpinejs');
 require('select2');
-
+const Swal = require('sweetalert2');
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+    //   toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
 
