76937b0466182fa7745aa339cec37ad831c464e5
h-alimohammadi <hosein.alimohammadi73@gmail.com>
2023-05-14 03:09:35 -0700

    create most visited for index page

---
+++ resources/views/livewire/home/home/home/most_visited.blade.php
@@ -0,0 +1,93 @@
+<section class="tns-carousel tns-controls-lg mt-5">
+    <h2 class="h3 text-center">محصولات پربازدید</h2>
+    <div class="swiper swiper_specials">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            @foreach (\App\Models\Product::with('category')->orderBy('view','ASC')->get() as $product)
+                <div class="swiper-slide">
+                    <div class="">
+                        <div class="card product-card">
+                            <button class="btn-wishlist btn-sm" type="button" data-bs-toggle="tooltip"
+                                data-bs-placement="left" title=""
+                                data-bs-original-title="اضافه کردن به علاقه مندی"
+                                aria-label="اضافه کردن به علاقه مندی"><i class="ci-heart"></i></button><a
+                                class="card-img-top d-block overflow-hidden" href="shop-single-v1.html"><img
+                                    src="/storage/{{ $product->img }}" alt="محصول"></a>
+                            <div class="card-body py-2 cart_body_product"><a class="product-meta d-block fs-xs pb-1"
+                                    href="#">{{ $product->category->title }}</a>
+                                <h3 class="product-title fs-sm"><a
+                                        href="shop-single-v1.html">{{ substr($product->title, 50) . '...' }}</a>
+                                </h3>
+                                <div class="d-flex justify-content-between">
+                                    <div class="product-price"><span class="text-accent">39.<small>50</small></span>
+                                    </div>
+                                    <div class="star-rating"><i class="star-rating-icon ci-star-filled active"></i><i
+                                            class="star-rating-icon ci-star-filled active"></i><i
+                                            class="star-rating-icon ci-star-filled active"></i><i
+                                            class="star-rating-icon ci-star-filled active"></i><i
+                                            class="star-rating-icon ci-star-filled active"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body ">
+                                {{-- <div class="text-center pb-2">
+                                    <div class="form-check form-option form-check-inline mb-2">
+                                        <input class="form-check-input" type="radio" name="size2" id="xs">
+                                        <label class="form-option-label" for="xs">XS</label>
+                                    </div>
+                                    <div class="form-check form-option form-check-inline mb-2">
+                                        <input class="form-check-input" type="radio" name="size2" id="s"
+                                            checked="">
+                                        <label class="form-option-label" for="s">S</label>
+                                    </div>
+                                    <div class="form-check form-option form-check-inline mb-2">
+                                        <input class="form-check-input" type="radio" name="size2" id="m">
+                                        <label class="form-option-label" for="m">M</label>
+                                    </div>
+                                    <div class="form-check form-option form-check-inline mb-2">
+                                        <input class="form-check-input" type="radio" name="size2" id="l">
+                                        <label class="form-option-label" for="l">L</label>
+                                    </div>
+                                </div> --}}
+                                <button
+                                    class="btn btn-primary btn-sm d-block w-100 mb-2 d-flex justify-content-center align-items-center"
+                                    type="button"><i class="ci-cart fs-sm me-1"></i>اضافه کردن به سبدخرید</button>
+                                <div class="text-center"><a class="nav-link-style fs-ms" href="#quick-view"
+                                        data-bs-toggle="modal"><i class="ci-eye align-middle me-1"></i>مشاهده</a></div>
+                            </div>
+                        </div>
+                        <hr class="d-sm-none">
+                    </div>
+                </div>
+            @endforeach
+            <div class="swiper-slide">
+                <div class="">
+                    <div class="card product-card">
+                        <a class="card-img-top d-block overflow-hidden  d-flex justify-content-center align-items-center fs-6"
+                            href="shop-single-v1.html">مشاهده همه ></a>
+                        <div class="card-body py-2 cart_body_product"><a class="product-meta d-block fs-xs pb-1"
+                                href="#"></a>
+                            <h3 class="product-title fs-sm "><a href="shop-single-v1.html"></a></h3>                            
+                        </div>
+                       
+                    </div>
+                    <hr class="d-sm-none">
+                </div>
+            </div>
+        </div>
+        <!-- If we need pagination -->
+        {{-- <div class="swiper-pagination1"></div> --}}
+
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+        <!-- If we need scrollbar -->
+    </div>
+
+</section>
+
+@section('script')
+    <script></script>
+@endsection
--- resources/views/livewire/home/home/index1.blade.php
+++ resources/views/livewire/home/home/index1.blade.php
@@ -4,6 +4,8 @@
     {{-- @include('livewire.home.home.home.categories') --}}
     @include('livewire.home.home.home.Products_grid')
     @include('livewire.home.home.home.featured_category')
+    @include('livewire.home.home.home.most_visited')
+
     @include('livewire.home.home.home.info_cards')
 
 </div>
