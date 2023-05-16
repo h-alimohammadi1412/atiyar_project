f7d27db50ceb48de7da9b52626813c68f4ec2203
h-alimohammadi <hosein.alimohammadi73@gmail.com>
2023-05-16 02:41:19 -0700

    create 404 page

--- app/Http/Livewire/Home/Home/Index.php
+++ app/Http/Livewire/Home/Home/Index.php
@@ -28,46 +28,102 @@ class Index extends Component
 
     public function render()
     {
-        auth()->loginUsingId(2);
-        $ip = Request::ip();
-        if (auth()->user()) {
-            $no = ModelsNotification::where('user_id', auth()->user()->id)->
-                where('type', 'ip')->get()->last();
-
-            if ($no != null) {
-
-                if ($no->ip != $ip) {
-
-                    $type = 'ip';
-                    $ip = Request::ip();
-                    // Notification::create([
-                    //     'user_id' => auth()->user()->id,
-                    //     'type' => $type,
-                    //     'sms' => 0,
-                    //     'email' => 0,
-                    //     'ip' => $ip,
-                    //     'system' => 1,
-                    //     'text' => ' هشدار: یک ورود موفق با آی پی ' . $ip . ' در سیستم ثبت شده است. ',
-                    // ]);
-                }
-
-            } elseif ($no == null) {
-
-                $type = 'ip';
-                $ip = Request::ip();
-                // Notification::create([
-                //     'user_id' => auth()->user()->id,
-                //     'type' => $type,
-                //     'sms' => 0,
-                //     'ip' => $ip,
-                //     'email' => 0,
-                //     'system' => 1,
-                //     'text' => ' هشدار: یک ورود موفق با آی پی ' . $ip . ' در سیستم ثبت شده است. ',
-                // ]);
-            }
-
-
+        if(!cache('categories')){
+            $categories = \App\Models\Category::where('parent_id', 0)->with('getChild.getChild')->get();
+            cache(['categories' =>  $categories ], now()->addDay(29));
         }
+        if(!cache('siteHeader')){
+            $siteHeader = \App\Models\SiteHeader::get();
+            cache(['siteHeader' =>  $siteHeader ], now()->addDay(29));
+        }
+        if(!cache('footerLinkTitle')){
+            $footerLinkTitle =\App\Models\FooterLinkTitle::with('getPage')->get();
+            cache(['footerLinkTitle' =>  $footerLinkTitle ], now()->addDay(29));
+        }
+        if(!cache('footerLinkOne')){
+            $footerLinkOne =\App\Models\FooterLinkOne::with('getPage')->get();
+            cache(['footerLinkOne' =>  $footerLinkOne ], now()->addDay(29));
+        }
+        if(!cache('footerInnerBox')){
+            $footerInnerBox =\App\Models\FooterInnerBox::with('getPage')->where('top', 1)->get();
+            cache(['footerInnerBox' =>  $footerInnerBox ], now()->addDay(29));
+        }
+        if(!cache('social')){
+            $social =\App\Models\Social::all();
+            cache(['social' =>  $social ], now()->addDay(29));
+        }
+        if(!cache('footerTitle')){
+            $footerTitle =\App\Models\FooterTitle::get();
+            cache(['footerTitle' =>  $footerTitle ], now()->addDay(29));
+        }
+        if(!cache('slider')){
+            $slider =\App\Models\Slider::all();
+            cache(['slider' =>  $slider ], now()->addDay(29));
+        }
+        if(!cache('specialProduct')){
+            $specialProduct =\App\Models\SpecialProduct::with(['product', 'category'])->get();
+            cache(['specialProduct' =>  $specialProduct ], now()->addDay(29));
+        }
+        if(!cache('productsGrid')){
+            $productsGrid =\App\Models\Product::orderBy('view','ASC')->limit(8)->get();
+            cache(['productsGrid' =>  $productsGrid ], now()->addDay(29));
+        }
+        if(!cache('featuredCategory')){
+            $categoryIndex = \App\Models\TitleCategoryIndex::find(1);
+            $featuredCategory =\App\Models\CategoryIndex::with('product')->where('title_id',$categoryIndex->id)->limit(12)->get();
+            cache(['featuredCategory' =>  $featuredCategory ], now()->addDay(29));
+        }
+        if(!cache('mostVisited')){
+            $mostVisited =\App\Models\Product::with('category')->orderBy('view','ASC')->get();
+            cache(['mostVisited' =>  $mostVisited ], now()->addDay(29));
+        }
+
+
+
+
+
+        // dd(cache('siteHeader'));
+
+        // auth()->loginUsingId(2);
+        // $ip = Request::ip();
+        // if (auth()->user()) {
+        //     $no = ModelsNotification::where('user_id', auth()->user()->id)->
+        //         where('type', 'ip')->get()->last();
+
+        //     if ($no != null) {
+
+        //         if ($no->ip != $ip) {
+
+        //             $type = 'ip';
+        //             $ip = Request::ip();
+        //             // Notification::create([
+        //             //     'user_id' => auth()->user()->id,
+        //             //     'type' => $type,
+        //             //     'sms' => 0,
+        //             //     'email' => 0,
+        //             //     'ip' => $ip,
+        //             //     'system' => 1,
+        //             //     'text' => ' هشدار: یک ورود موفق با آی پی ' . $ip . ' در سیستم ثبت شده است. ',
+        //             // ]);
+        //         }
+
+        //     } elseif ($no == null) {
+
+        //         $type = 'ip';
+        //         $ip = Request::ip();
+        //         // Notification::create([
+        //         //     'user_id' => auth()->user()->id,
+        //         //     'type' => $type,
+        //         //     'sms' => 0,
+        //         //     'ip' => $ip,
+        //         //     'email' => 0,
+        //         //     'system' => 1,
+        //         //     'text' => ' هشدار: یک ورود موفق با آی پی ' . $ip . ' در سیستم ثبت شده است. ',
+        //         // ]);
+        //     }
+
+
+        // }
 
 
 
